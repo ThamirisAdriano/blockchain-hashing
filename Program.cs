c1901afb8a5b6098ee7071715b8f133199df1f84
thamiris <tsadriano.band.com.br>
2024-03-13 07:15:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "Blockchain é fascinante!";
+        Console.WriteLine($"Hash Original: {input}");
+
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder hash = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hash.Append(hashBytes[i].ToString("x2"));
+            }
+            Console.WriteLine($"Hash SHA256: {hash.ToString()}");
+        }
+    }
+}
+
